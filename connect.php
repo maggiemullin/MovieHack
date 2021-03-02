191e687cda052298ccfbb9ba7965526dd6e14f58
maggiemullin <maggiemullin@gmail.com>
2021-03-02 17:27:40 -0500

    update database connection

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 try{
 
-    $dsn = 'mysql:host=localhost;dbname=movie_hack';
-    $username = 'root';
-    $password = 'root'; //mamp users
+    $dsn = 'mysql:host=172.31.22.43;dbname=Mullin100104425';
+    $username = 'Mullin100104425';
+    $password = 'UQabtxmMN5'; //mamp users
 
     $db = new PDO($dsn, $username, $password);
     //echo "<p> Successfully connected </p>";
