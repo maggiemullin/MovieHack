d5ee065ab20ae99debf85706638f2ec0d5c84377
maggiemullin <maggiemullin1@gmail.com>
2021-03-07 23:07:19 -0500

    d

--- connect.php
+++ connect.php
@@ -3,8 +3,7 @@ try{
 
    $dsn = 'mysql:host=172.31.22.43;dbname=Mullin100104425';
     $username = 'Mullin100104425';
-    $password = 'UQabtxmMN5'; //mamp users
-
+    $password = 'UQabtxmMN5';
 }
 catch(PDOException $e){
     echo "<p> This did not work </p>";
