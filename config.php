34db41be405e3406d335674eade2d8f2b17db2bb
maggiemullin <71662878+maggiemullin@users.noreply.github.com>
2021-04-09 10:57:22 -0400

    test

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
   // Defined your site key and secret key
-  define('SITEKEY','6Leaf6AaAAAAAHy9TXzzNrQTvL-FXqzI3IFL9NLI');
+  define('SITEKEY','6LdK-aIaAAAAAFPMF5p-ywMBcrnt5ygH35Z0Gj3A');
   
-  define('SECRETKEY', '6Leaf6AaAAAAAChWAvguxmSrU0NRvteYFmd56AeO');
+  define('SECRETKEY', '6LdK-aIaAAAAAIhpL5mvLtAeom7kMt7_ovrALE_5');
   ?>
